d2125b24436b29cc044cb831ea83a122613d96d0
Dmytro <dmytro@email.com>
2025-03-08 22:34:44 +0200

    update

--- src/sources/base.py
+++ src/sources/base.py
@@ -7,9 +7,6 @@ from typing import Optional
 
 from models.paper import PaperMetadata
 
-logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-)
 logger = logging.getLogger(__name__)
 
 
